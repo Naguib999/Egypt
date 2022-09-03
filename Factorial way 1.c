a819c2f35f135a82274320f9ac939b721e3ae8d4
Naguib999 <naguib.bashapik@gmail.com>
2022-09-03 20:15:40 +0200

    an update has happened

---
+++ Factorial way 1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int k,i,num=1;
+    printf("Enter any number");
+    scanf("%d",&k);
+
+   for(i=1;i<=k;i++)
+   {
+
+
+    num*=i;
+
+
+
+   }
+
+
+
+  printf("%d",num);
+
+
+}
+
